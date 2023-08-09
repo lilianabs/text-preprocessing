49c1ce9b9380d64ed7d5c16e7a0ca2d7472ce2e4
lilianabs <lilianabsmath@gmail.com>
2023-08-08 22:50:22 -0600

    Add extract_user_mentions function

--- src/text_preprocessing/extract.py
+++ src/text_preprocessing/extract.py
@@ -1,7 +1,7 @@
 import re
 
 def extract_hashtags(txt: str):
-    """Extracts hastags from a given string
+    """Extracts hastags from a given text
 
     Args:
         txt (str): Text 
@@ -10,3 +10,15 @@ def extract_hashtags(txt: str):
         list: List that contains all hashtags
     """
     return re.findall(r'(?<!\S)#\S+|\S+#(?!\S)', txt)
+
+
+def extract_user_mentions(txt: str):
+    """Extracts user mentions from a given text
+
+    Args:
+        txt (str): Text
+
+    Returns:
+        list: List that contains all user mentions
+    """
+    return re.findall(r'@\w+', txt)
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,4 +1,5 @@
 from src.text_preprocessing.extract import extract_hashtags
+from src.text_preprocessing.extract import extract_user_mentions
 
 def test_extract_hashtag():
     txt =  "This text contains hashtags #be-happy and #test"
@@ -6,4 +7,11 @@ def test_extract_hashtag():
     extracted_hashtags = extract_hashtags(txt)
     
     assert extracted_hashtags == hashtags_from_txt
+    
+def test_extract_user_mentions():
+    txt =  "This text contains @user, @second_user, and @user3  mentions"
+    user_mentions_from_txt = ['@user', '@second_user', '@user3']
+    extracted_user_mentions = extract_user_mentions(txt)
+
+    assert extracted_user_mentions == user_mentions_from_txt
     
