6295354ad0061a22bf8589690af85efefb36faf8
lilianabs <lilianabsmath@gmail.com>
2023-08-08 22:13:12 -0600

    Renamed package

---
+++ src/text_preprocessing/extract.py
@@ -0,0 +1,12 @@
+import re
+
+def extract_hashtags(txt: str):
+    """Extracts hastags from a given string
+
+    Args:
+        txt (str): Text 
+
+    Returns:
+        list: List that contains all hashtags
+    """
+    return re.findall(r'#\w+', txt)
