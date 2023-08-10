645bd3eb7fdc7cd1e7edd3f3622c8b1ce2e23247
lilianabs <lilianabsmath@gmail.com>
2023-08-09 23:45:36 -0600

    Format code

--- src/text_preprocessing/remove.py
+++ src/text_preprocessing/remove.py
@@ -1,17 +1,18 @@
 import re
 
+
 def remove_hashtags(txt: str):
     """Removes hastags from a given text
 
     Args:
-        txt (str): Text 
+        txt (str): Text
 
     Returns:
         txt (str): Text without hashtags
     """
-    txt_without_hashtags = re.sub(r'(?<!\S)#\S+|\S+#(?!\S)', 
-                                  '', txt)
-    return re.sub(' +', ' ', txt_without_hashtags)
+    txt_without_hashtags = re.sub(r"(?<!\S)#\S+|\S+#(?!\S)", "", txt)
+    return re.sub(" +", " ", txt_without_hashtags)
+
 
 def remove_user_mentions(txt: str):
     """Removes user mentions from a given text
@@ -22,8 +23,9 @@ def remove_user_mentions(txt: str):
     Returns:
         txt (str): Text without user mentions
     """
-    txt_without_user_mentions = re.sub(r'@\w+', '', txt)
-    return re.sub(' +', ' ', txt_without_user_mentions)
+    txt_without_user_mentions = re.sub(r"@\w+", "", txt)
+    return re.sub(" +", " ", txt_without_user_mentions)
+
 
 def remove_url(txt: str):
     """Extracts url's from a given text
@@ -33,5 +35,5 @@ def remove_url(txt: str):
     Returns:
         list: List that contains urls
     """
-    txt_without_urls = re.sub(r'https?://\S+', '', txt)
-    return re.sub(' +', ' ', txt_without_urls)
+    txt_without_urls = re.sub(r"https?://\S+", "", txt)
+    return re.sub(" +", " ", txt_without_urls)
